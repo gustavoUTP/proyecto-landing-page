a5ce148b64655150e91b8503f6286cf352f466eb
gustavoUTP <u22212913@utp.edu.pe>
2025-09-05 10:45:21 -0500

    cambios en el index.html y en main.js

---
+++ js/main.js
@@ -0,0 +1,5 @@
+// Elementos del formulario
+const form = document.querySelector('#miFormulario');
+const nombreInput = document.querySelector('#nombre');
+const correoInput = document.querySelector('#correo');
+const mensaje 
