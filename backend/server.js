526676da0fc9c2684af24e3d25490134b281c7a1
piero12004 <pierobajonero.12004@gmail.com>
2025-10-23 16:20:53 -0500

    cambios back para deploy

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+require('dotenv').config();
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
 
 // ===== Conexión a MongoDB =====
-mongoose.connect('mongodb://127.0.0.1:27017/formularioDB')
-  .then(() => console.log('✅ Conectado a MongoDB'))
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('✅ Conectado a MongoDB Atlas'))
   .catch(err => console.error('❌ Error al conectar a MongoDB:', err));
-
-
 // ===== MODELOS =====
 
 // --- Modelo de cursos ---
